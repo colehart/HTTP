2f8462e802b2a1f6d8de8896a3f2c0785a2f2bb6
Cole Hart <cole@colehart.com>
2018-04-03 17:50:15 -0600

    Iteration 0 - removed bloat and added counter.

--- lib/experiment.rb
+++ lib/experiment.rb
@@ -1,32 +1,20 @@
 require 'socket'
 tcp_server = TCPServer.new(9292)
-puts "Ready for a request"
-
-#client represents return address
-client = tcp_server.accept
-
-#this is them opening the letter
-request_lines = []
-
-while (line = client.gets) && !line.chomp.empty?
-  request_lines << line.chomp
+counter = 0
+loop do
+  connection = tcp_server.accept
+  request_lines = []
+  line = connection.gets.chomp
+  until line.empty?
+    line = connection.gets.chomp
+    request_lines << line
+  end
+  request_lines.inspect
+  output = "Hello, World! (#{counter})."
+  headers = ["http/1.1 200 ok"]
+  connection.puts headers
+  connection.puts"\n"
+  connection.puts output
+  connection.close
+  counter += 1
 end
-
-puts "Got this request:"
-puts request_lines.inspect
-
-puts "Sending response."
-response = "<pre>" + request_lines.join("\n") + "</pre>"
-output = "<html><head></head><body>#{response}</body></html>"
-headers = ["http/1.1 200 ok",
-          "date: #{Time.now.strftime('%a, %e %b %Y %H:%M:%S %z')}",
-          "server: ruby",
-          "content-type: text/html; charset=iso-8859-1",
-          "content-length: #{output.length}\r\n\r\n"].join("\r\n")
-
-client.puts headers
-client.puts output
-
-puts ["Wrote this response:", headers, output].join("\n")
-client.close
-puts "\nResponse complete, exiting."
