ac8819f05542f317f7616e9b1bfd56f2749aecfc
Cole Hart <cole@colehart.com>
2018-04-04 15:39:08 -0600

    skipped

--- test/server_test.rb
+++ test/server_test.rb
@@ -5,12 +5,13 @@ require 'faraday'
 
 class ServerTest < Minitest::Test
   # works as unit test if no integration tests running
-  # def test_it_exists
-  #   server = Server.new(9292)
-  #   assert_instance_of Server, server
-  # end
+  def test_it_exists
+    server = Server.new(9292)
+    assert_instance_of Server, server
+  end
 
   def test_connection_starts_as_nil
+    skip
     server = Faraday.get('http://localhost:9292/')
     assert_nil server.connection
   end
